49df1728d2453f8e1f8d9d75baa92dfa4524e9e6
danhtran94 <danh.tt1294@gmail.com>
2020-06-19 17:07:58 +0700

    more i18n

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,13 +1,14 @@
 import { defineComponent } from "vue";
-import { RouteView } from "lib/curi-router-plugin/index";
 import { jsx } from "utils/jsx";
+import { i18n } from "lib/lingui-plugin/index";
+import { RouteView } from "lib/curi-router-plugin/index";
 
 import { Layout } from "src/components/layout";
 
 export const App = defineComponent({
   setup(props, ctx) {
     return () => (
-      <Layout title="Simple counter app">{() => <RouteView />}</Layout>
+      <Layout title={i18n._("app_name")}>{() => <RouteView />}</Layout>
     );
   },
 });
--- src/locale/vi/messages.js
+++ src/locale/vi/messages.js
@@ -1,1 +1,1 @@
-/* eslint-disable */export default{languageData:{"plurals":function(n,ord){if(ord)return n==1?"one":"other";return"other"}},messages:{"hello":"Xin ch\xE0o!"}};
+/* eslint-disable */export default{languageData:{"plurals":function(n,ord){if(ord)return n==1?"one":"other";return"other"}},messages:{"app_name":"\u0111\xE9m s\u1ED1","hello":"Xin ch\xE0o!"}};
